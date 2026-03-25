811caf92da6b294af25de6ea7847f5b69315bb61
sarumathiinbaraj-prog <sarumathiinbaraj@gmail.com>
2026-03-25 12:38:29 +0530

    commit

---
+++ javaa/Day2/CharArrayToString.java
@@ -0,0 +1,15 @@
+package Day2;
+public class CharArrayToString {
+    public static void main(String[] args) {
+        char[] ch={'a','b','c'};
+        System.out.print(new String(ch));
+    }
+}
+
+// public class CharArrayToString {
+//     public static void main(String[] args) {
+//         StringBuilder s=new StringBuilder();
+//         char[] ch={'a','b','c'};
+//         System.out.print(s.append(ch).toString());
+//     }
+// }
